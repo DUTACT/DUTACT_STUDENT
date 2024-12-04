b0c6bd4a1e9ee35873c25336a1036e4cfd66c76a
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-12-04 22:54:56 +0700

    feat: set up receive notification flow

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,13 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './contexts/app.context.tsx'
 import store from './redux/store.ts'
 import { Provider } from 'react-redux'
+import { connectWebSocket } from './services/websocket.ts'
+import { addNotification } from './redux/slices/notificationsSlice.ts'
+import { CONFIG } from './constants/config.ts'
+
+connectWebSocket(CONFIG.WEBSOCKET_URL, (data) => {
+  store.dispatch(addNotification(data))
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import modalConfirmReducer from './slices/modalConfirm'
+import notificationsReducer from './slices/notificationsSlice'
+
 
 import { configureStore } from '@reduxjs/toolkit'
 
 const reducer = combineReducers({
   modalConfirm: modalConfirmReducer,
+  notifications: notificationsReducer,
 })
 
 const store = configureStore({
@@ -19,6 +22,7 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 
 export const modalConfirmState = (state: RootState) => state.modalConfirm
+export const notificationsState = (state: RootState) => state.notifications
 
 export type AppDispatch = typeof store.dispatch
 
